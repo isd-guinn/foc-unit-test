4339c17885d5851c8c5c8c464c40c8c29bbb81a3
SandersISD <yhshum@connect.ust.hk>
2024-10-05 13:10:50 +0800

    Tested code with working magnetic sensor

--- foc-control/foc-control.ino
+++ foc-control/foc-control.ino
@@ -4,31 +4,60 @@
 
 MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
 
-void setup() {
+TaskHandle_t task_sensor_get_status_HANDLE = NULL;
+void task_sensor_get_status( void * pvParameters )
+{
+  for( ;; )
+  {
+    sensor.update();
+    vTaskDelay(100);
+  }
+}
+
+TaskHandle_t task_sensor_print_status_HANDLE = NULL;
+void task_sensor_print_status( void * pvParameters )
+{
+  for( ;; )
+  {
+    Serial.print("STATUS\t||\tangle: ");
+    Serial.println(sensor.getAngle());
+    Serial.print("STATUS\t||\tvelocity: ");
+    Serial.println(sensor.getVelocity());
+    vTaskDelay(1000 /portTICK_PERIOD_MS);
+  }
+}
+
+void setup() 
+{
   
   Serial.begin(115200);
 
   Wire.setClock(400000);
 
   // Start I2C
-  while (Wire.begin(SDA_PIN,SCL_PIN)){
-    Serial.prinln("PREP: Begin I2C");
+  while (!(Wire.begin(SDA_PIN,SCL_PIN)))
+  {
+    Serial.println("PREP\t||\tBegin I2C");
     vTaskDelay(1000/portTICK_PERIOD_MS);
-    Serial.prinln("ERROR  || I2C Cant Init")
+    Serial.println("ERROR\t||\tI2C Cant Init");
   }
-  Serial.prinln("DONE   || I2C Ready")
+  Serial.println("DONE\t||\tI2C Ready");
 
   // Start sensor
   sensor.init(&Wire);
 
-  Serial.prinln("DONE   || Sensor Ready");
+  Serial.println("DONE\t||\tSensor Ready");
+
+  //pin tasks
+  xTaskCreate( task_sensor_get_status, "get_status", 2048, NULL, 1, &task_sensor_get_status_HANDLE );
+  //configASSERT( task_sensor_get_status_HANDLE );
+
+  xTaskCreate( task_sensor_print_status, "print_status", 2048, NULL, 2, &task_sensor_print_status_HANDLE );
+  //configASSERT( task_sensor_print_status_HANDLE );
+
+  vTaskDelay(1000 /portTICK_PERIOD_MS);
 }
 
-void loop() {
-  sensor.update();
-
-  Serial.print("STATUS  || angle: ");
-  Serial.println("");
-
-
+void loop() 
+{
 }
